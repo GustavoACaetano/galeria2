87017b44c0ed4c7865e06278134e36dc3905e835
20211TIIMI0060 <gustavo.guguvat@gmail.com>
2023-05-05 11:05:43 -0300

    05/05 - Passo 8 criar o arquivo xml file paths

--- app/src/main/java/caetano/alves/galeria2/MainActivity.java
+++ app/src/main/java/caetano/alves/galeria2/MainActivity.java
@@ -1,9 +1,14 @@
 package caetano.alves.galeria2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,4 +20,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.tbMain); // Capturando a toolbar criada
         setSupportActionBar(toolbar); // Setando a toolbar na página main
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater(); // Cria um inflador de menu
+        inflater.inflate(R.menu.main_activity_tb, menu); // Passa a tela e o menu que o inflador de menu colocará as opções de menu
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) { // Executa quando o item da toolbar for selecionado
+        switch (item.getItemId()) { // Pega o id do item clicado
+            case R.id.opCamera: // Caso seja o item de camera
+                dispatchTakePictureIntent(); // Executa o codigo que dispara a camera
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
 }
--- app/src/main/java/caetano/alves/galeria2/PhotoActivity.java
+++ app/src/main/java/caetano/alves/galeria2/PhotoActivity.java
@@ -1,9 +1,15 @@
 package caetano.alves.galeria2;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -14,5 +20,29 @@ public class PhotoActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.tbPhoto); // Capturando a toolbar criada
         setSupportActionBar(toolbar); // Setando a toolbar na página photoActivity
+
+        ActionBar actionbar = getSupportActionBar(); // Capturando a actionbar que acabamos de definir acima
+        actionbar.setDisplayHomeAsUpEnabled(true); // Habilitando o botão de voltar
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater(); // Cria um inflador de menu
+        inflater.inflate(R.menu.photo_activity_tb, menu); // Passa a tela e o menu que o inflador de menu colocará as opções de menu
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) { // Executa quando um item de menu for clicado
+        switch (item.getItemId()) { // Seleciona o id do item
+            case R.id.opShare: // Caso seja o item de compartilhar
+                sharePhoto(); // Chama o método que compartilha a foto
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+            }
+        }
+
+
 }
